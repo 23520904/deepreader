e41b27ea1971048546b1d621147cb2c040b71002
23520904 <23520904@gm.uit.edu.vn>
2026-04-03 23:38:05 +0700

    chore: init multi-module project via Spring Initializr

---
+++ business-service/src/test/java/com/deepreader/business_service/BusinessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepreader.business_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web-module/src/test/java/com/deepreader/web_module/WebModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepreader.web_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
